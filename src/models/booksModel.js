2e9cc27afe6873f4395eee5f6686a281f6382f2b
himanshu <hsupare313@gmail.com>
2022-07-06 13:21:07 +0530

    userSchema

--- src/models/booksModel.js
+++ src/models/booksModel.js
@@ -2,17 +2,44 @@ const mongoose = require('mongoose')
 
 const bookSchema = new mongoose.Schema({
 
-    title: { type: String, required: true, unique: true, trim: true },
-    excerpt: { type: String, required: true, trim: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'user' },
-    ISBN: { type: String, required: true, unique: true },
-    category: { type: String, required: true },
-    subcategory: { type: [String], required: true },
-    reviews: { type: Number, default: 0},
-    isDeleted: { type: Boolean, default: false }
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    excerpt: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'user'
+    },
+    ISBN: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    subcategory: {
+        type: [String],
+        required: true
+    },
+    reviews: {
+        type: Number,
+        default: 0
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
 
-},
-    { timestamps: true }
-);
+}, { timestamps: true });
 
 module.exports = mongoose.model('book', bookSchema);
